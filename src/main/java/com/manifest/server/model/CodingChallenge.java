199b8e189c32d7588ef04968ef49cee1fc3213b1
rileylittlefield <rlittlefield@manifestcorp.com>
2017-02-21 15:32:19 -0500

    adds codingChallenge model and codingChallengeRepository

---
+++ src/main/java/com/manifest/server/model/CodingChallenge.java
@@ -0,0 +1,66 @@
+package com.manifest.server.model;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class CodingChallenge {
+
+	@Id 
+	@GeneratedValue 
+	private Long id;
+	
+	private String name; 
+	private String description;
+	private String difficulty;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "date_created", insertable = true, updatable = false)
+	private Date dateCreated;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getDifficulty() {
+		return difficulty;
+	}
+
+	public void setDifficulty(String difficulty) {
+		this.difficulty = difficulty;
+	}
+
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+
+	public void setDateCreated(Date dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+}
---
+++ src/main/java/com/manifest/server/repository/CodingChallengeRepository.java
@@ -0,0 +1,7 @@
+package com.manifest.server.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.manifest.server.model.CodingChallenge;
+
+public interface CodingChallengeRepository extends CrudRepository<CodingChallenge, Long> { }
