1bc3ba5bd24ab4cfcf0bfd8564a6f3ecaeae3a41
rileylittlefield <rlittlefield@manifestcorp.com>
2017-02-22 11:57:38 -0500

    adds basic challenge index

--- scripts/src/components/ChallengeIndex/_ChallengeIndex.jsx
+++ scripts/src/components/ChallengeIndex/_ChallengeIndex.jsx
@@ -6,6 +6,33 @@ export default class ChallengeIndex extends React.Component {
   }
 
   render() {
-    return <div>THIS IS THE CHALLENGE INDEX</div>;
+    return (
+      <section id="coding-challenges">
+        <h1>Coding Challenges</h1>
+
+        <div id="search-filters">
+          <div class="difficulty-filters">
+            <label htmlFor="easy-checkbox">Easy</label>
+            <input type="checkbox" id="easy-checkbox"/>
+
+            <label htmlFor="medium-checkbox">Medium</label>
+            <input type="checkbox" id="medium-checkbox"/>
+
+            <label htmlFor="hard-checkbox">Hard</label>
+            <input type="checkbox" id="hard-checkbox"/>
+          </div>
+
+          <input type="text" placeholder="filter by name" />
+        </div>
+
+        <div id="coding-challenge-list">
+          <ul>
+            <li>This is a sample list item</li>
+            <li>This is a sample list item</li>
+            <li>This is a sample list item</li>
+          </ul>
+        </div>
+      </section>
+    );
   }
 }
