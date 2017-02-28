5489d966156175d455079db30988a07f2babd548
rileylittlefield <rlittlefield@manifestcorp.com>
2017-02-28 12:02:33 -0500

    makes unit test outline for CodingChallenge Component

--- scripts/spec/components/CodingChallenge/CodingChallenge.spec.js
+++ scripts/spec/components/CodingChallenge/CodingChallenge.spec.js
@@ -2,4 +2,18 @@ import CodingChallenge from "../../../src/components/CodingChallenge/_CodingChal
 
 describe("CodingChallenge", () => {
   it("is defined", () => expect(CodingChallenge).toBeDefined());
+
+  describe("#componentDidMount", () => {
+    xit("places a subscription callback into the redux store");
+    xit("makes a request to the backend to get the coding challenge in question");
+  });
+
+  describe("#componentWillUnmount", () => {
+    xit("unsubscribes from the redux store");
+  })
+
+  describe("#setCodingChallenge", () => {
+    xit("obtains the correct codingChallenge Instance from the redux store");
+    xit("sets the appropriate state on the CodingChallengeComponent");
+  });
 });
