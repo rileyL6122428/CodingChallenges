aed0eaa3621e6cf932c35a4000b40599c7ee1a9d
rileylittlefield <rlittlefield@manifestcorp.com>
2017-02-28 11:35:57 -0500

    adds solution editting to coding challenge page

--- scripts/src/components/ChallengeIndividual/SolutionSubmission.jsx
+++ scripts/src/components/ChallengeIndividual/SolutionSubmission.jsx
@@ -4,12 +4,27 @@ export default class SolutionSubmission extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.state = { solution: "" };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.state.solution = "Class Solution { \n\n" +
+      "\tpublic " + nextProps.methodSignature + "{ \n" +
+      "\t\t\n" +
+      "\t} \n\n" +
+      "}";
+  }
+
+  updateSolution(editEvent) {
+    let updatedSolution = editEvent.target.value;
+    this.setState({ solution: updatedSolution });
   }
 
   render() {
     return (
       <div id="solution-submission">
-        <textarea readOnly value={ this.props.methodSignature } rows="10" cols="50" />
+        <textarea onChange={this.updateSolution.bind(this)} value={ this.state.solution } rows="10" cols="50" />
         <button>Submit</button>
       </div>
     );
