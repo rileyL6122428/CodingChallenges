c9645834027b31882354beffdde63c41693bb6dd
rileylittlefield <rlittlefield@manifestcorp.com>
2017-02-28 10:30:33 -0500

    adds backendApi method to grab a codingChallenge

--- scripts/src/backendApi/codingChallenges.js
+++ scripts/src/backendApi/codingChallenges.js
@@ -1,13 +1,22 @@
 import $ from 'jquery';
 import Store from '../redux/store.js';
-import { addChallenges } from '../redux/actions/codingChallenge.actions.js';
+import { addChallenges, addChallenge } from '../redux/actions/codingChallenge.actions.js';
 
 export default {
   getCodingChallenges: () => {
     $.ajax({
-      url: "./api/coding-challenges",
+      url: "/api/coding-challenges",
       type: "GET",
       success: (response) => Store.dispatch(addChallenges(response)),
+    });
+  },
+
+  getCodingChallenge: (id) => {
+    $.ajax({
+      url: "/api/coding-challenge/" + id,
+      type: "GET",
+      success: (response) => Store.dispatch(addChallenge(response)),
+      error: () => console.log("AN ERROR OCCURRED")
     });
   }
 };
