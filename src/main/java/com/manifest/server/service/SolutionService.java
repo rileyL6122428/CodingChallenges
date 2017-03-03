19654a85ddebe99dcb7adcbe21e2b432328c1d48
rileylittlefield <rlittlefield@manifestcorp.com>
2017-03-03 16:24:56 -0500

    refactors SolutionService

--- src/main/java/com/manifest/server/service/SolutionService.java
+++ src/main/java/com/manifest/server/service/SolutionService.java
@@ -23,30 +23,35 @@ public class SolutionService {
 	
 	public SolutionGrade reviewSolution(SolutionSubmissionRequest submitSolutionRequest)  {
 		try {
-			long challengeId = submitSolutionRequest.getChallengeId();
-			String sourceCode = submitSolutionRequest.getSourceCode();
-			
-			CodingChallenge codingChallenge = codingChallengeRepository.findOne(challengeId);
-			
+			CodingChallenge codingChallenge = codingChallengeRepository.findOne(submitSolutionRequest.getChallengeId());			
 			SolutionSubmission solutionSubmission = new SolutionSubmission();
-			solutionSubmission.setSourceCode(sourceCode);
+			
+			solutionSubmission.setSourceCode(submitSolutionRequest.getSourceCode());
 			solutionSubmission.setChallengeName(codingChallenge.getName());
 			solutionSubmission.setMethodName(codingChallenge.getName());
-			
-			List<ParameterType> parameterTypes = codingChallenge.getParameterTypes();
-			Class<?>[] parameterClasses = new Class<?>[parameterTypes.size()];
-			for (int idx = 0; idx < parameterClasses.length; idx++) {
-				parameterClasses[idx] = Class.forName(parameterTypes.get(idx).getLibraryName());
-			}
-			solutionSubmission.setParameterClasses(parameterClasses);
+			solutionSubmission.setParameterClasses(convertTypesToClasses(codingChallenge.getParameterTypes()));
 			
 			return solutionReviewer.reviewSubmission(solutionSubmission);
 			
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
-			SolutionGrade grade = new SolutionGrade();
-			grade.setPassesTests(false);
-			return grade;
+			return failingGrade();
 		}
+	}
+	
+	private Class<?>[] convertTypesToClasses(List<ParameterType> parameterTypes) throws ClassNotFoundException {
+		Class<?>[] parameterClasses = new Class<?>[parameterTypes.size()];
+		
+		for (int idx = 0; idx < parameterClasses.length; idx++) {
+			parameterClasses[idx] = Class.forName(parameterTypes.get(idx).getLibraryName());
+		}
+		
+		return parameterClasses;
+	}
+	
+	private SolutionGrade failingGrade() {
+		SolutionGrade grade = new SolutionGrade();
+		grade.setPassesTests(false);
+		return grade;
 	}
 }
