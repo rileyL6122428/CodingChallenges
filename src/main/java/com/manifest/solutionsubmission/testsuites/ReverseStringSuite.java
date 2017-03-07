b724d485e2f2c6fa9562c5865d6366bd8ec094bf
rileylittlefield <rlittlefield@manifestcorp.com>
2017-03-07 12:47:41 -0500

    adds basic support for reverse string challenge

--- src/main/java/com/manifest/solutionsubmission/testsuites/ReverseStringSuite.java
+++ src/main/java/com/manifest/solutionsubmission/testsuites/ReverseStringSuite.java
@@ -1,9 +1,24 @@
 package com.manifest.solutionsubmission.testsuites;
 
+import com.manifest.solutionsubmission.SolutionTest;
 import com.manifest.solutionsubmission.TestSuite;
 
 public class ReverseStringSuite extends TestSuite {
 	public ReverseStringSuite() {
 		super("reverseString");
+	}
+	
+	{
+		addTest(new SolutionTest<String>(new Object[]{""}, ""));
+		
+		addTest(new SolutionTest<String>(new Object[]{"a"}, "a"));
+		
+		addTest(new SolutionTest<String>(new Object[]{"abc"}, "cba"));
+		
+		addTest(new SolutionTest<String>(new Object[]{"abcb"}, "bcba"));
+		
+		addTest(new SolutionTest<String>(new Object[]{"abcba"}, "abcba"));
+		
+		addTest(new SolutionTest<String>(new Object[]{"abcbaaa"}, "aaabcba"));
 	}
 }
