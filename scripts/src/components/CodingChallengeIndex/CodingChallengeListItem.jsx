003e31f649c3d34b06a3fea6555d35820c66dfee
rileylittlefield <rlittlefield@manifestcorp.com>
2017-03-08 20:21:56 -0500

    adds basic styling to index page

--- scripts/src/components/CodingChallengeIndex/CodingChallengeListItem.jsx
+++ scripts/src/components/CodingChallengeIndex/CodingChallengeListItem.jsx
@@ -11,15 +11,15 @@ export default class CodingChallengeListItem extends React.Component {
     return (
       <li className="coding-challenge">
         <Link to={"coding-challenge/" + this.props.challenge.id}>
-          <div className="challenge-difficulty">
+          <div className="challenge-difficulty challenge-info">
             { this.props.challenge.difficulty }
           </div>
 
-          <div className="challenge-name">
+          <div className="challenge-name challenge-info">
             { this.props.challenge.name }
           </div>
 
-          <div className="challenge-date-created">
+          <div className="challenge-date-created challenge-info">
             { DateUtils.monthDayYear(this.props.challenge.dateCreated) }
           </div>
         </Link>
